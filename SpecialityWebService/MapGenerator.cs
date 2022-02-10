e20dc49de26b841c6a1568b6b248a620e4ec3623
Djixify <tobias.floor@gmail.com>
2022-02-10 19:34:19 +0100

    Added initial ISS project

---
+++ SpecialityWebService/MapGenerator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpecialityWebService
+{
+    public class MapGenerator
+    {
+        public int test { get; set; }
+    }
+}

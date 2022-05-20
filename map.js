c82f464b129b985b45fcaa21f3674207f7667cb0
Tobias Floor <tzf>
2022-05-20 20:16:50 +0200

    Seemingly working implementation

--- map.js
+++ map.js
@@ -1,10 +1,7 @@
 //https://localhost:44342
 //http://localhost:8082
 var servers = 
-    ["https://80.210.64.251:8082",
-     "https://localhost:8082",
-     "http://80.210.64.251:8082",
-     "http://localhost:8082",
+    ["https://networkspeciality.dk",
      "https://localhost:44342"]
 var server = servers[4]; 
 
